0fa967a4dbcd1c934c6a672bf57bc7806ff32bd1
SoleneDc <duchamp.solene.pro@gmail.com>
2018-02-16 19:31:11 +0100

    Huge cleaning

--- programme_3_changed.py
+++
@@ -1,7 +0,0 @@
-# Programme 3: programme suivant
-# 1 : while X <= 0 :
-#     then 2 :
-#            if Y <= 0 :
-#            then 3 : X := X - Y
-#            else 4 : X := X + Y
-# 5 : then : return X, Y
